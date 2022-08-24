200495717a8318c031161d7ba2172d5503fccc19
PythonTattzee <jl##^%hdolD@74>
2022-08-24 20:56:39 +0200

    changing database

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import requests
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
 
 
