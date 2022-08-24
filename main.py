58ec8236f901a365dc140806e10e4e21b5fc90ac
PythonTattzee <jl##^%hdolD@74>
2022-08-24 20:07:24 +0200

    changing database

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ class Comment(db.Model):
 # db.create_all()
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
     all_movies = Movie.query.order_by(desc(Movie.rating)).all()
     all_comments = Comment.query.all()
